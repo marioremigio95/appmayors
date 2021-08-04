d73c039e40209c77cd437d8bf93a65725df2358c
marioremigio95 <mario.remigio@gmail.com>
2021-08-04 13:19:48 -0300

    councilors 1976 elections results

---
+++ data-raw/councilor_results_1976.R
@@ -0,0 +1,19 @@
+## code to prepare `councilor_results_1976` dataset goes here
+
+councilor_results_1976 <-
+  map_df(.x = list.files("data-raw", pattern = "results_76"),
+         .f = ~read_csv2(paste0("data-raw/", .))) %>%
+  janitor::clean_names() %>%
+  rename(name_mun_1970 = municipios,
+         uf_1970 = sigla,
+         year = ano) %>%
+  mutate(valid_votes = arena + mdb,
+         arena_votes_share = arena/valid_votes,
+         mdb_votes_share = mdb/valid_votes,
+         brancos_share = brancos/valid_votes,
+         nulos_share = nulos/valid_votes) %>%
+  mutate(name_mun_1970 = stringr::str_to_lower(name_mun_1970),
+         name_mun_1970 = stringr::str_replace_all(name_mun_1970, "-", " "),
+         name_mun_1970 = stringi::stri_trans_general(name_mun_1970, "Latin-ASCII"))
+
+usethis::use_data(councilor_results_1976, overwrite = TRUE)
