9a73b811cebb7461e324a05d4ebf4293e2907bf8
marioremigio95 <mario.remigio@gmail.com>
2021-08-04 13:15:41 -0300

    new data commiting

---
+++ R/data.R
@@ -0,0 +1,13 @@
+
+#' IBGE's 1970 demographic census aggregated by municipality.
+#'
+#' This dataset contains descriptive statistics for the Brazilian municipalities calculated from the
+#' IBGE's 1970 demographic census microdata.
+#'
+#' @format O formato é:
+#' \describe{
+#'   \item{coluna}{descricao}
+#'   ...
+#' }
+#' @source \url{https://www.kaggle.com/shivamb/netflix-shows?select=netflix_titles.csv}
+"census70ibge"
