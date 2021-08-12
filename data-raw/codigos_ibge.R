a06344722607b0e4e49a5080b9596d6dce4b63b7
marioremigio95 <mario.remigio@gmail.com>
2021-08-12 11:37:17 -0300

    municipalities' names into lower case

--- data-raw/codigos_ibge.R
+++ data-raw/codigos_ibge.R
@@ -3,6 +3,10 @@
 codigos_ibge <- readr::read_csv("data-raw/codigos_ibge.csv") %>%
   janitor::clean_names() %>%
   rename(cod_mun_ibge = codigo1,
-         cod_mun_ibge6 = codigo2)
+         cod_mun_ibge6 = codigo2) %>%
+  mutate(name_mun = stringr::str_to_lower(municipio),
+         name_mun = stringr::str_replace_all(name_mun, "-", " "),
+         name_mun = stringi::stri_trans_general(name_mun, "Latin-ASCII"))
+
 
 usethis::use_data(codigos_ibge, overwrite = TRUE)
